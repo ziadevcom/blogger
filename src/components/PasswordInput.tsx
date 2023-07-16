1f588d400f5cc0e6f9316d1ede2ed1c687a9079b
= <=>
2023-07-16 14:45:05 +0500

    misc: remove sensitive files

---
+++ src/components/PasswordInput.tsx
@@ -0,0 +1,28 @@
+import { InputGroup, InputRightElement, Button, Input } from "@chakra-ui/react";
+import { Eye, EyeOff } from "lucide-react";
+import { useState, forwardRef } from "react";
+
+export const PasswordInput = forwardRef(function PasswordInput(
+  props: any,
+  ref: any
+) {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  return (
+    <InputGroup size="md">
+      <Input
+        pr="4.5rem"
+        type={show ? "text" : "password"}
+        {...props}
+        ref={ref}
+      />
+
+      <InputRightElement width="4.5rem" height="100%">
+        <Button h="1.75rem" size="sm" onClick={handleClick} variant="link">
+          {show ? <EyeOff /> : <Eye />}
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+});
