cecab2cd7868596f5e55ce8a554e0a335a589845
Zia Ahmad <me@ziadev.com>
2023-07-28 02:53:40 +0500

    misc: update db schema, add some types etc

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,20 +1,21 @@
-// Extending next-auth types to get rid of erros from next auth config
-// Help from https://stackoverflow.com/questions/74425533/property-role-does-not-exist-on-type-user-adapteruser-in-nextauth
+// // Extending next-auth types to get rid of erros from next auth config
+// // Help from https://stackoverflow.com/questions/74425533/property-role-does-not-exist-on-type-user-adapteruser-in-nextauth
 
 import { DefaultUser } from "next-auth";
 
 declare module "next-auth" {
   interface Session {
-    user?: DefaultUser & { active: boolean };
+    user?: DefaultUser & { active: boolean; id: string };
   }
 
-  interface User extends DefaultUser {
+  interface User {
+    id: string;
     active: boolean;
   }
 }
-
 declare module "next-auth/jwt" {
   interface JWT {
+    id: string;
     active: boolean;
   }
 }
---
+++ src/utils/getFullURL.ts
@@ -0,0 +1,5 @@
+import "server-only";
+
+export function getFullURL(pathname: string) {
+  return process.env.HOST + "/" + pathname;
+}
