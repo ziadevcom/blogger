907486d583648f99e629644ae8fcae58750f8c88
Zia Ahmad <me@ziadev.com>
2023-08-03 01:09:28 +0500

    feat: get all posts api route

---
+++ src/app/api/posts/route.ts
@@ -0,0 +1,27 @@
+export const dynamic = "force-dynamic"; // Fixes "Dynamic headers usage error"
+
+import { getServerSession } from "next-auth";
+import { NextRequest, NextResponse } from "next/server";
+import { authOptions } from "../auth/[...nextauth]/route";
+import { prisma } from "@/db/prisma.client";
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    console.log(session, req);
+
+    if (!session?.user) throw Error("You are not logged in.");
+
+    const posts = await prisma.post.findMany({
+      where: { userId: session.user.id },
+    });
+
+    console.log(posts);
+
+    return NextResponse.json({ error: null, posts });
+  } catch (error: any) {
+    console.log(error);
+    return NextResponse.json({ error: error.message });
+  }
+}
