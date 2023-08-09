300a122722f47bcb3d023c77760066de9c2820ef
Zia Ahmad <me@ziadev.com>
2023-08-09 14:28:34 +0500

    feat: finish update post api route

--- src/app/api/post/route.ts
+++ src/app/api/post/route.ts
@@ -4,27 +4,7 @@ import { prisma } from "@/db/prisma.client";
 import { formatYupErrors } from "@/utils/formatYupErrors";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
-
-const postSchema = yup.object({
-  title: yup.string().trim().required(),
-  content: yup.string().trim().required(),
-  slug: yup
-    .string()
-    .trim()
-    .matches(
-      /^[a-zA-Z0-9-]+$/,
-      "Post URL can only contain alphabets, numbers, and dashes."
-    )
-    .required(),
-  featured_image: yup
-    .string()
-    .trim()
-    .matches(
-      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
-      "Invalid image URL."
-    ),
-  status: yup.string().trim().oneOf(["public", "draft", "trash"]).required(),
-});
+import { postSchema } from "@/yupSchemas/blogPostSchema";
 
 export async function POST(req: NextRequest) {
   try {
@@ -75,6 +55,69 @@ export async function POST(req: NextRequest) {
     });
 
     return NextResponse.json(post);
+  } catch (error: any) {
+    console.log(error);
+
+    if (error instanceof yup.ValidationError) {
+      return NextResponse.json(formatYupErrors(error), { status: 400 });
+    }
+
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
+
+export async function PUT(req: NextRequest) {
+  try {
+    const session = await getServerSession(authOptions);
+
+    if (!session || !session.user) {
+      return NextResponse.json(
+        { error: "You are not logged in." },
+        { status: 401 }
+      );
+    }
+    const formBody = await req.json();
+
+    await postSchema.validate(formBody, {
+      abortEarly: false,
+    });
+
+    // const { title, content, slug, featured_image, status } = formBody;
+    const { slug, id: postId, ...postData } = formBody;
+
+    const blog = await prisma.blog.findFirst({
+      where: { userId: session.user.id },
+    });
+
+    if (!blog) throw new Error("You don't have a blog.");
+
+    const urlTaken = await prisma.post.findFirst({
+      where: { slug: slug, blogId: blog.id },
+    });
+
+    console.log({ urlTaken });
+
+    if (!urlTaken) {
+      // postData.slug = `${formBody.title.replace(
+      //   " ",
+      //   "-"
+      // )}-${crypto.randomUUID()}`;
+      postData.slug = slug;
+    }
+
+    const post = await prisma.post.findFirst({
+      where: { id: postId },
+    });
+
+    console.log(formBody);
+    if (!post) throw new Error("Post does not exist.");
+
+    const updatedPost = await prisma.post.update({
+      where: { id: post.id },
+      data: postData,
+    });
+
+    return NextResponse.json(updatedPost);
   } catch (error: any) {
     console.log(error);
 
---
+++ src/yupSchemas/blogPostSchema.ts
@@ -0,0 +1,23 @@
+import * as yup from "yup";
+
+export const postSchema = yup.object({
+  title: yup.string().trim().required(),
+  content: yup.string().trim().required(),
+  slug: yup
+    .string()
+    .trim()
+    .matches(
+      /^[a-zA-Z0-9-]+$/,
+      "Post URL can only contain alphabets, numbers, and dashes."
+    )
+    .required(),
+  featured_image: yup
+    .string()
+    .trim()
+    .matches(
+      /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/,
+      "Invalid image URL."
+    )
+    .nullable(),
+  status: yup.string().trim().oneOf(["public", "draft", "trash"]).required(),
+});
